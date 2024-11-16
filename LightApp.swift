1c1dcf7cb6cf8b27d644785e9b5cce10a6d604a0
Luna Yin <69876438+luna-yin@users.noreply.github.com>
2024-11-16 16:39:14 -0500

    Add files via upload

---
+++ LightApp.swift
@@ -0,0 +1,17 @@
+//
+//  LightApp.swift
+//  Light
+//
+//  Created by Lucy Yin on 1/1/22.
+//
+
+import SwiftUI
+
+@main
+struct LightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
